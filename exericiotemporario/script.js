7c4e09f87adfcc6b9141d27433caa382a3455ff6
BrunoWeezyy <neidinidnid@hotmail.com>
2024-03-04 22:00:47 -0300

    ajustei os cars -botao ver estorias no meio

--- exericiotemporario/script.js
+++ exericiotemporario/script.js
@@ -1,19 +1,16 @@
-function Testando(){
-let numeroum = document.getElementById(text1);
-
-
-function text1 (){
-    
-
-    
+function focusCard(card) {
+    card.querySelector('.nomes').style.opacity = 1;
 }
-function text1 (){
-    
-    
+function focusCard(card) {
+    card.querySelector('.buttonVermais').style.opacity = 1;
 }
 
-function btn (){
-    
-    
+// Função para exibir o botão "Ver Estória" ao passar o mouse sobre o card
+function addFocusButton(card) {
+    card.querySelector('.buttonVermais').style.display = 'inline-block'; /* Exibe o botão */
 }
+
+// Função para ocultar o botão "Ver Estória" ao retirar o mouse do card
+function removeFocusButton(card) {
+    card.querySelector('.buttonVermais').style.display = 'none'; /* Oculta o botão */
 }
