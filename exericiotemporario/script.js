15b1929c2e1f4afa3ea74ab451aa69f4b110899f
BrunoWeezyy <neidinidnid@hotmail.com>
2024-03-05 00:25:40 -0300

    desfocando img com css

--- exericiotemporario/script.js
+++ exericiotemporario/script.js
@@ -1,16 +1,4 @@
-function focusCard(card) {
-    card.querySelector('.nomes').style.opacity = 1;
-}
-function focusCard(card) {
-    card.querySelector('.buttonVermais').style.opacity = 1;
-}
-
-// Função para exibir o botão "Ver Estória" ao passar o mouse sobre o card
-function addFocusButton(card) {
-    card.querySelector('.buttonVermais').style.display = 'inline-block'; /* Exibe o botão */
-}
-
-// Função para ocultar o botão "Ver Estória" ao retirar o mouse do card
-function removeFocusButton(card) {
-    card.querySelector('.buttonVermais').style.display = 'none'; /* Oculta o botão */
-}
+function toggleBlur() {
+    var portfolioContainer = document.querySelector('img_gallery');
+    portfolioContainer.classList.toggle('blur');
+  }
