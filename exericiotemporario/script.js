355836efc85f2be4c659a97daf5ad52775e1740f
BrunoWeezyy <neidinidnid@hotmail.com>
2024-03-09 20:30:33 -0300

    componente busca acionado js

--- exericiotemporario/script.js
+++ exericiotemporario/script.js
@@ -1,4 +1,26 @@
-function toggleBlur() {
-    var portfolioContainer = document.querySelector('img_gallery');
-    portfolioContainer.classList.toggle('blur');
+function procurarCampeao() {
+  var inputValor = document.getElementById('text1').value.toLowerCase();
+  var resultadoBusca = document.getElementById('resultadoBusca');
+  resultadoBusca.innerHTML = '';
+
+  if (inputValor) {
+    var imagens = document.querySelectorAll('.img_gallery');
+
+    imagens.forEach(function(imagem) {
+      var nomeCampeao = imagem.alt.toLowerCase();
+      if (nomeCampeao.includes(inputValor)) {
+        var cloneArticle = imagem.closest('article').cloneNode(true);
+        resultadoBusca.appendChild(cloneArticle);
+      }
+    });
+
+    if (resultadoBusca.innerHTML === '') {
+      resultadoBusca.textContent = 'Nenhum campeão encontrado.';
+    }
+  } else {
+    resultadoBusca.textContent = '';
   }
+}
+
+
+
